661801b418ceb605c7a4fddbb5c5337e27324de0
ADITYA VERMA <adityav1304@gmail.com>
2024-10-18 20:09:28 +0530

    a

--- src/App.js
+++ src/App.js
@@ -10,17 +10,40 @@ import { AudioProvider } from './Component/Global/AudioContext/AudioContext';
 import CountdownTimer from './Component/Global/Timer/CountdownTimer';
 
 export default function App() {
+  // Load the Devfolio SDK
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://apply.devfolio.co/v2/sdk.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <>
       <NavBar />
       <Landing />
-      <CountdownTimer /> {/* Add the CountdownTimer component */}
+      <CountdownTimer />
       <Grid />
       <AudioProvider>
         <GetGame />
       </AudioProvider>
       <Slider />
       <Poster />
+
+      {/* Apply with Devfolio Button */}
+      <div style={{ display: 'flex', justifyContent: 'center', margin: '20px 0' }}>
+        <div 
+          className="apply-button" 
+          data-hackathon-slug="hack-of-duty" 
+          data-button-theme="light"
+          style={{ height: '44px', width: '312px' }}
+        ></div>
+      </div>
+
       <Footer />
     </>
   );
--- src/Component/Main/Poster/Poster.js
+++ src/Component/Main/Poster/Poster.js
@@ -1,28 +1,10 @@
 import './Poster.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import React from 'react';
 
 export default function Poster() {
-  React.useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://apply.devfolio.co/v2/sdk.js';
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="poster-container">
       <img src="./images/main page/poster/poster.gif" alt="Poster" className="poster-image" />
-      <div 
-        className="apply-button" 
-        data-hackathon-slug="SRMSIGKDD" 
-        data-button-theme="light" 
-        style={{ height: '44px', width: '312px',marginTop:"100px" }}
-      ></div>
     </div>
   );
 }
